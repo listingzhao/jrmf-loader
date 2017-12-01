df9e83f0912881d8e6c848bfb8f0bc9deff85fab
Listing <listing.zhao@gmail.com>
2017-12-01 17:51:57 +0800

    修改

---
+++ README.md
@@ -0,0 +1,3 @@
+# jrmf-loader
+
+A webpack loader for processing .vue file before vue-loader
---
+++ src/style-loader.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const utils = require('loader-utils')
+
+module.exports = function (source) {
+  this.cacheable()
+  const _this = this
+  const config = this.jrmf || utils.getLoaderConfig(this, 'jrmf')
+
+  if (!config.plugins || !config.plugins.length) {
+    return source
+  }
+
+  config.plugins.forEach(function (plugin) {
+    // style-parser
+    if (plugin.name === 'jrmf-style-parser') {
+      if (plugin.fn) {
+        source = plugin.fn.call(_this, source)
+      }
+    }
+  })
+
+  if (config.options.jrmfDev) {
+    if (/App\.vue$/.test(this.resourcePath)) {
+      source = source.replace(/~jrmf\/src/g, '.')
+    } else {
+      source = source.replace(/~jrmf\/src/g, '..')
+    }
+  }
+
+  return source
+}
